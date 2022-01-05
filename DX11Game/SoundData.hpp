b39921fd81b78d87186b7ea35e9976063179139b
Kozuki <Rouxff424@gmail.com>
2022-01-05 22:47:01 +0900

    サウンドデータ.hppにコメントを追加しました

--- DX11Game/SoundData.hpp
+++ DX11Game/SoundData.hpp
@@ -1,5 +1,14 @@
 // BGM/SE定義 (SoundData.hpp)
 
+
+//**************************************************************
+//	開発履歴
+//	音ファイルを追加しています。
+//	※印は上月大地が独断で採用している音ファイルです
+//	編集者：上月大地
+//**************************************************************
+
+
 LPCWSTR g_pszFileBGM[MAX_BGM] = {
 	L"BGM_Title000.mp3",	// タイトル		※仮
 	L"BGM_Game000.mp3",		// ゲーム		※仮
@@ -15,7 +24,7 @@ LPCWSTR g_pszFileSE[MAX_SE] = {
 	L"SE_Select000.mp3",	// セレクト移動 ※仮
 	L"SE_Jump000.mp3",		// ジャンプ		※仮
 	L"SE_Landing000.mp3",	// 着地			※仮
-	L"SE_WallBreak000.mp3",	// 壁破壊 
+	L"SE_WallBreak000.mp3",	// 壁破壊		※仮
 	L"SE_PDamage000.mp3",	// ダメージ
 	L"SE_Heal000.mp3",		// 回復音
 	L"SE_Beep000.mp3",		// 警告音		※仮
